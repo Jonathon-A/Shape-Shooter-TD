071440048ac6e97908e8581ba0b6e76ce9e1640c
Jonathon-A <axfordjc@gmail.com>
2021-08-22 14:34:42 +0100

    added letter y to a comment

--- Shape_Shooter_TD/src/shape_shooter_td/Shape_Shooter_TDAssets.java
+++ Shape_Shooter_TD/src/shape_shooter_td/Shape_Shooter_TDAssets.java
@@ -30,7 +30,7 @@ import javax.swing.event.ChangeEvent;
 
 public abstract class Shape_Shooter_TDAssets {
     
-    //Director of project on user's computer
+    //Directory of project on user's computer
     final String UserDirectory = System.getProperty("user.dir") + "\\dist\\";
     //Array of all tile icons
     final protected Icon[] AssetsTileIconArray = {
